58bfe7124cf7271920f0978b8bd5e875a89c7912
mrbrowasi <mrbrowasi1432005@gmail.com>
2026-07-16 19:40:16 +0500

    Modify service worker caching strategy
    Updated the service worker to cache additional resources.

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 self.addEventListener('install', (e) => {
   e.waitUntil(
     caches.open('myapp-v1').then((cache) => {
@@ -10,7 +9,7 @@ self.addEventListener('install', (e) => {
         '/192image.png',
         '/512image.png',
       ]);
-    })
+    }
   );
 });
 
@@ -18,24 +17,3 @@ self.addEventListener('fetch', (e) => {
   e.respondWith(
     caches.match(e.request).then((response) => response || fetch(e.request))
   );
-=======
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open('myapp-v1').then((cache) => {
-      return cache.addAll([
-        '/',
-        '/index.html',
-        '/style.css',
-        '/script.js',
-        '/icon-192.png'
-      ]);
-    })
-  );
-});
-
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => response || fetch(e.request))
-  );
->>>>>>> 1432a9c9dcd62cfd77bf7b9a71695e9af3639ca5
-});
